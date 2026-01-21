6916f4b4cbf5f59f33c98aba76462d0329745800
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-01-21 23:14:07 +0430

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex bg-indigo-500 p-4"> 
+           <h1 className="font-bold text-white text-3xl pl-10">Country Explorer</h1>
+        </header>
+    )
+}
