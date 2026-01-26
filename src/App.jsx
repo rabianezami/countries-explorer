e77aa15db27e9df2f38e7266c2cfaf5491c76d03
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-01-26 21:01:41 +0430

    feat(search): Add SearchBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
+import { useState } from 'react'
 import Header from './components/Header'
+import SearchBar from './components/SearchBar'
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("")
   
   return (
     <>
      <Header />
+     <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       
     </>
   )
