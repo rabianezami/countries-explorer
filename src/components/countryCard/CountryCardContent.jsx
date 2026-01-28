0102bf908f19471e518155790ba139f2605daddc
rabia zia nezami <rabianezami786@gail.com>
2026-01-28 11:57:42 +0430

    Add CountryCardContent component

---
+++ src/components/countryCard/CountryCardContent.jsx
@@ -0,0 +1,20 @@
+export default function CountryCardContent({ flag, name, region, population }) {
+    return (
+        <div className="p-10 grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
+            <div className="flex justify-center sm:justify-start">
+                <img
+                    src={flag}
+                    alt={name}
+                    className="w-28 h-16 object-cover mx-auto mb-2 rounded"
+                />
+            </div>
+            <div className="text-center sm:text-left">
+                <h2 className="font-bold text-lg mb-1">{name}</h2>
+                <p className="text-gray-600 text-sm">Region: {region}</p>
+                <p className="text-gray-600 text-sm">
+                    Population: {population?.toLocaleString()}
+                </p>
+            </div>
+        </div>
+    )
+}
