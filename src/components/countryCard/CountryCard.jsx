2e0d8618b7ec8c981c0cb241fcc223ccf7e11355
rabia zia nezami <rabianezami786@gail.com>
2026-01-28 11:56:35 +0430

    Add reuseable CountryCard component

---
+++ src/components/countryCard/CountryCard.jsx
@@ -0,0 +1,9 @@
+
+
+export default function CountryCard({ children }) {
+  return (
+    <div className="bg-white border border-gray-200 rounded-xl overflow-hidden mt-10 mx-auto max-w-xl">
+      {children}
+    </div>
+  );
+}
